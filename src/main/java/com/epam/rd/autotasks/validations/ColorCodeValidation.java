9cbb45bcaba381112d9d2a542a216a4a87abb117
Work <vanda70601s@mail.ru>
2023-10-19 22:11:10 +0300

    Commit

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -5,16 +5,13 @@ import java.util.regex.Pattern;
 
 
 public class ColorCodeValidation {
-
     public static boolean validateColorCode(String color) {
         if (color == null || color.isBlank()) return false;
 
 
         Pattern pattern = Pattern.compile("^#([a-fA-F\\d]{6}|[a-fA-F\\d]{3})$");
-Matcher matcher = pattern.matcher(color);
-return matcher.matches();
-
-
+        Matcher matcher = pattern.matcher(color);
+        return matcher.matches();
 
 
     }
